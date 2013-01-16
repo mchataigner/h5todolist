b054a48e2e9d86371c7fabba419083fda5e405d0
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2013-01-16 18:11:59 +0100

    angular js #5

--- application.js
+++ application.js
@@ -1,94 +1,78 @@
+function TodoListCtrl($scope){
 
-function clear(){
-	localStorage.clear();
-	load();
-}
+	$scope.todo={ title: "", description: "", url: "", deadline: "", done: false }
+	var urlparser=RegExp("^([a-z]*:?\/\/).*");
 
-function postaction(){
-	var name=document.getElementById("title").value;
-	var description=document.getElementById("description").value;
-	var url=document.getElementById("url").value;
-	var deadline=document.getElementById("deadline").value;
-	var done=document.getElementById("done").checked;
-	if(name == "" && url != "") name = url;
-	if(name == "")return;
-	var data=loadData();
-	var todo={"title":name,"description":description,"url":url,"deadline":deadline,"done":done};
-	data.push(todo);
-	saveData(data);
-	var form=document.getElementById("form");
-	form.reset();
-	load();
-	console.log("add "+JSON.stringify(todo));
-}
+	$scope.add = function(){
+		todo = this.todo;
 
-function loadData(){
-	var collection=localStorage.getItem("stock");
-	if(collection!=null){
-		return JSON.parse(collection);
+		if(todo.title == "" && todo.url == "")return;
+		
+		console.log("add");
+		
+		if(todo.title == "")todo.title = todo.url;
+		if(todo.url != "" && !urlparser.test(todo.url))todo.url="//"+todo.url;
+		this.todos.push(todo);
+		this.todo={ title: "", description: "", url: "", deadline: "", done: false }
+		this.save();
+		this.focus();
 	}
-	else return [];
-}
 
-function saveData(data){
-	localStorage.setItem("stock",JSON.stringify(data));
-}
-
-function remove(i){
-	var data=loadData();
-	var todo=data.splice(i,1)[0];
-	saveData(data);
-	load();
-	console.log("remove "+JSON.stringify(todo));
-}
-
-function update(i,status){
-	var data=loadData();
-	var todo=data.splice(i,1)[0];
-	todo.done=status;
-	data.splice(i,0,todo);
-	saveData(data);
-	load();
-	console.log("status update to "+status+" "+JSON.stringify(todo));
-}
-
-function load(){
-	var content=document.getElementById("content");
-	var collection=localStorage.getItem("stock");
-	if(collection!=null){
-		var data=JSON.parse(collection);
-		var contentString="";
-		for (var i = data.length - 1; i >= 0; i--) {
-			var style="";
-			var checked="";
-			var name="";
-			var date="";
-			var arrow = "";
-			if(data[i].done){
-				style="done";
-				checked="checked='"+data[i].done+"' ";
-			}
-
-			if(data[i].url == "") name = data[i].title;
-			else name = "<a href='//"+data[i].url+"'>"+data[i].title+"</a>";
-
-			if(data[i].deadline != "") date=" ("+data[i].deadline+") ";
-
-			if(data[i].deadline != "" || data[i].description != "") arrow = " => ";
-
-			contentString+="<span class='"+style+"'>"+name+arrow+data[i].description+date+
-				" <input type='checkbox' name='done' value='done' "+checked+" onchange='update("+i+","+!data[i].done+")'/></span> "+
-				"<a href='javascript:remove("+i+")'>remove</a> "+
-				"<a href='https://www.google.com/search?q="+data[i].title+"'>google("+data[i].title+")</a>"+
-				"<br/>";
-		};
-		content.innerHTML=contentString;
+	$scope.update = function(){
+		console.log("update");
+		//this.todos[index].done = !this.todos[index].done;
+		//$scope.$apply();
+		//saveData(todos);
+		this.save();
 	}
-	else
-		content.innerHTML="";
-	document.getElementById("title").focus();
+
+	$scope.remove = function(index){
+		console.log("remove");
+		this.todos.splice(index,1);
+		//saveData(todos);
+		this.save();
+		//this.load();
+		this.$apply();
+		this.focus();
+	}
+
+	$scope.load = function(){
+		console.log("load");
+		this.todos=(JSON.parse(localStorage.getItem("stock"))||[]);
+		this.focus();
+	}
+
+	$scope.save = function(){
+		console.log("save");
+		localStorage.setItem("stock",JSON.stringify(this.todos));
+	}
+
+	$scope.focus = function(){
+		document.getElementById("title").focus();
+	}
+
+	window.remove = function(i){$scope.remove(i);}
+
+	window.clear = function(){
+		console.log("clear");
+		$scope.todos=[];
+		$scope.save();
+		$scope.$apply();
+		$scope.focus();
+	}
+
+	window.update=function(){$scope.update;}
+
+	window.reload = function(){
+		$scope.load();
+		$scope.$apply();
+	}
+
+	/*window.onload = function(){
+		$scope.load();
+	}*/
+
+	window.addEventListener("storage", reload,false);
+
+	$scope.load();
 }
-
-window.onload=load;
-
-window.addEventListener("storage",load,false);
